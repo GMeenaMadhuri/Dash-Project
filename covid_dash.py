bbdc74afa858397e848eb8960f34a18c4b993561
GMeenaMadhuri <33513894+GMeenaMadhuri@users.noreply.github.com>
2020-08-19 19:10:58 -0500

    Update covid_dash.py

--- covid_dash.py
+++ covid_dash.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0)
-import plotly.graph_objects as go
-
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -18,7 +16,7 @@ print(df[:5])
 # App layout
 app.layout = html.Div([
 
-    html.H1("Covid-19 Dashboard for some of the US-States with Dash", style={'text-align': 'center'}),
+    html.H1("COVID-19 analysis for some of the states in USA using Dash framework ", style={'text-align': 'center'}),
 
     dcc.Dropdown(id="slct_state",
                  options=[
